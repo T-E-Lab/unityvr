Fix rotational velocity calculation

unityvr/analysis/posAnalysis.py
@@ -126,7 +126,7 @@ def computeVelocities(posDf, convf = 10, window=7, order=3):
 
     # add derrived parameter to positional dataframe
     posDf['vT'] = np.hypot(np.gradient(posDf.x.values*convf), np.gradient(posDf.y.values*convf))*(1/posDf.dt)
-    posDf['vR'] = np.gradient(np.unwrap(posDf.angle.values))
+    posDf['vR'] = np.gradient(np.unwrap(posDf.angle.values, period=360))*(1/posDf.dt)
 
     posDf['vT_filt'] = savgol_filter(posDf.vT, window, order)
     posDf['vR_filt'] = savgol_filter(posDf.vR, window, order)